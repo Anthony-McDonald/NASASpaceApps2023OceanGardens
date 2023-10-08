2367f1bd6dec8c85c798eae76f013b23a220d278
Anthony McDonald <89093671+Anthony-McDonald@users.noreply.github.com>
2023-10-08 10:59:02 +0100

    phytoplankton alankton animation

---
+++ myscripts.js
@@ -0,0 +1,16 @@
+console.log("js is jsing")
+var img = document.getElementById('img');
+var img_array = ['../../imgs/algae1.png', '../../imgs/algae2.png',
+'../../imgs/algae3.png','../../imgs/algae4.png'
+,'../../imgs/algae5.png','../../imgs/algae6.png'
+,'../../imgs/algae7.png'
+,'../../imgs/algae8.png','../../imgs/algae9.png'
+,'../../imgs/algae10.png','../../imgs/algae11.png'];
+
+function setImage(obj)
+{
+    console.log("detected")
+	var value = obj.value;
+	img.src = img_array[value];
+    
+}
